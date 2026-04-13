dfdaa2d41c0d857f96b85bc7cca1a0c07b35972c
MagiCat <corinavicky07@gmail.com>
2026-04-13 17:23:57 -0400

    Add files via upload

---
+++ Practica-02/J. Last Digit of Factorial.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long int n;
+    
+    cin>>n;
+    
+    switch(n) {
+        case 0: 
+            cout<<1<<endl;
+            break;
+        case 1: 
+            cout<<1<<endl;
+            break;
+        case 2: 
+            cout<<2<<endl;
+            break;
+        case 3: 
+            cout<<6<<endl;
+            break;
+        case 4: 
+            cout<<4<<endl;
+            break;
+        default:
+            cout<<0<<endl;
+    }
+
+    
+    return 0;
+}
